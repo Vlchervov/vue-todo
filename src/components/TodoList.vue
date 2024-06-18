4b159bded1a8754631e16092ba53f3c9d309a02c
Vladislav <vladislav.chervov@actonica.ru>
2024-06-18 18:01:17 +0700

    small fix

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,29 +9,34 @@ const todoListStore = useTodoListStore()
             <button v-for="redaction of todoListStore.redactionMode" @click="todoListStore.changeRedactionMode">{{
                 redaction.name }}</button>
         </div>
-        <ul class="tasks-list">
-            <li v-for="todo of todoListStore.tasks">
-                <label :class="todo.class" :for="todo.id" @click="() => {
-                    if (todoListStore.checkMode) todoListStore.toggleCompleted(todo.id)
-                }">
-                    {{ todo.value }}
-                    <button v-if="!todoListStore.checkMode" class="delete-button"
-                        @click="todoListStore.deleteTask(todo.id)" />
-                </label>
-                <input type="checkbox" :id="todo.id" />
-            </li>
-        </ul>
-        <form class="add-task-container" v-for="state of todoListStore.InputState"
-            :on-change="todoListStore.checkInput(state.taskValue)">
-            <p v-if="todoListStore.totalCountTasks <= 0">Список задач пуст. Добавьте задачу, чтобы она отобразилась в
-                списке.</p>
+        <div class="tasks-list-container">
+            <ul class="tasks-list">
+                <li v-for="todo of todoListStore.tasks">
+                    <label :for="todo.id" @click="() => {
+                if (todoListStore.checkRedactionMode) todoListStore.toggleCompleted(todo.id)
+            }">
+                        <button :class="todo.class" class="check-button" v-if="todoListStore.checkRedactionMode" />
+                        <button v-if="!todoListStore.checkRedactionMode" class="delete-button"
+                            @click="todoListStore.deleteTask(todo.id)" />{{ todo.value }}
+                    </label>
+                </li>
+                <p v-if="todoListStore.totalCountTasks <= 0">Список задач пуст. Добавьте задачу, чтобы она отобразилась
+                    в
+                    списке.</p>
+            </ul>
+        </div>
+        <button class="add-task-button" v-if="todoListStore.checkRedactionMode"
+            @click="todoListStore.changeAddTaskHiddenState()">Добавить
+            задачу</button>
+        <div v-if="todoListStore.checkAddTaskHidden" class="add-task-container"
+            v-for="state of todoListStore.InputState" :on-change="todoListStore.checkInput(state.taskValue)">
             <textarea type="text" :maxlength="18" v-model="state.taskValue"
                 placeholder="Введите наименование задачи..." />
             <p v-if="state?.error.message">{{ state.error.message }}</p>
             <div>
                 <button type="button" @click="todoListStore.addNewTask(state.taskValue)">Добавить</button>
-                <button>Отмена</button>
+                <button @click="todoListStore.changeAddTaskHiddenState()">Отмена</button>
             </div>
-        </form>
+        </div>
     </main>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -15,6 +15,10 @@ export const useTodoListStore = defineStore('todoListStore', {
 
         redactionMode: [
             { redaction: false, name: "Править" }
+        ],
+
+        addTaskHidden: [
+            { hidden: true }
         ]
     }),
     getters: {
@@ -22,8 +26,12 @@ export const useTodoListStore = defineStore('todoListStore', {
             return this.tasks.length;
         },
 
-        checkMode() {
+        checkRedactionMode() {
             return this.redactionMode.findIndex(el => el.redaction);
+        },
+
+        checkAddTaskHidden() {
+            return this.addTaskHidden.findIndex(el => el.hidden);
         }
     },
     actions: {
@@ -34,7 +42,7 @@ export const useTodoListStore = defineStore('todoListStore', {
         },
         deleteTask(id) {
             this.tasks = this.tasks.filter(el => el.id !== id);
-         
+
         },
         checkInput(value) {
             const newError = this.InputState.findIndex(el => el.error.message === el.error.message);
@@ -56,13 +64,18 @@ export const useTodoListStore = defineStore('todoListStore', {
             } else {
                 this.InputState[newError].error.message = "Поле должно быть заполнено!";
             }
-         return [...this.tasks]
+            return [...this.tasks]
         },
 
         changeRedactionMode() {
             const newRedaction = this.redactionMode.findIndex(el => el.redaction === el.redaction);
             this.redactionMode[newRedaction].redaction = !this.redactionMode[newRedaction].redaction;
             this.redactionMode[newRedaction].name = this.redactionMode[newRedaction].redaction === true ? "Отмена" : "Править";
+            this.addTaskHidden[0].hidden = true;
+        },
+
+        changeAddTaskHiddenState() {
+            this.addTaskHidden[0].hidden = !this.addTaskHidden[0].hidden;
         }
     }
 })
